ef7521bdaebc4ebbc06dbda788b91500178ae811
Alexander Burachkov <zicio@yandex.ru>
2021-12-14 00:48:46 +0400

    Updated

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -7,32 +7,27 @@ export default class DOM {
     }
   }
 
-  static renderPopUp(e) {
+  static add(e) {
     e.preventDefault();
     if (e.target.classList.contains('list__add-submit')) {
-      const popUp = '<div class=popup><form action="" class="form" id="form"><div class="form__group"><label class="form__label" for="=name">Название</label><input type="text" class="form__name input" id="name" required=""></div><div class="form__group"><label class="form__label" for="price">Стоимость</label><input type="number" class="form__price input" id="price" min="1"required=""></div></form><div class="form__button-container"><button type="submit" class="form__save form__button">Сохранить</button><button type="reset" class="form__reset form__button">Отмена</button></div></div>';
-      e.target.closest('.list').insertAdjacentHTML('beforeend', popUp);
+      DOM.renderPopUp();
     }
   }
 
-  static saveProduct(e) {
-    e.preventDefault();
+  static saveProduct(e, index) {
     if (e.target.classList.contains('form__save')) {
+      e.preventDefault();
       const form = e.target.closest('.popup').querySelector('.form');
-      const newProductData = [[...form.elements].map(({ id, value }) => ({ id, value }))];
+      const newProductData = [...form.elements].map(({ id, value }) => ({ id, value }));
       const popUp = e.target.closest('.popup');
-      DOM.renderNewProduct(newProductData);
       popUp.remove();
-      if (!localStorage.productData) {
-        localStorage.setItem('productData', JSON.stringify(newProductData));
-        return;
-      }
-      const productData = JSON.parse(localStorage.productData);
-      for (const product of newProductData) {
-        productData.push(product);
-      }
-      localStorage.setItem('productData', JSON.stringify(productData));
+      DOM.saveData(newProductData);
+      DOM.renderProducts();
     }
+  }
+
+  static saveData(data) {
+    localStorage.setItem(`${data[0].value}`, JSON.stringify(data));
   }
 
   static resetForm(e) {
@@ -43,11 +38,46 @@ export default class DOM {
     }
   }
 
-  static renderNewProduct(productData) {
+  static completeForm(e) {
+    const inputsOfForm = document.querySelector('.form').elements;
+    const productName = e.target.closest('.list__product').querySelector('.product__name').textContent;
+    const product = JSON.parse(localStorage.getItem(productName));
+    for (let i = 0; i < inputsOfForm.length; i++) {
+      inputsOfForm[i].value = product[i].value;
+    }
+  }
+
+  static deleteProduct(e) {
+    if (e.target.classList.contains('delete-button')) {
+      const productName = e.target.closest('.list__product').querySelector('.product__name').textContent;
+      e.target.closest('.list__product').remove();
+      localStorage.removeItem(productName);
+    }
+  }
+
+  static renderPopUp() {
+    const popUpEl = '<div class=popup><form action="" class="form" id="form"><div class="form__group"><label class="form__label" for="=name">Название</label><input type="text" class="form__name input" id="name" required=""></div><div class="form__group"><label class="form__label" for="price">Стоимость</label><input type="number" class="form__price input" id="price" min="1"required=""></div></form><div class="form__button-container"><button type="submit" class="form__save form__button">Сохранить</button><button type="reset" class="form__reset form__button">Отмена</button></div></div>';
+    document.querySelector('.list').insertAdjacentHTML('beforeend', popUpEl);
+  }
+
+  static renderProduct(productData) {
     const newRowTable = document.querySelector('.list__table > tbody');
-    for (const el of productData) {
-      const ProductName = el[0].value;
-      const ProductPrice = el[1].value;
+    const ProductName = productData[0].value;
+    const ProductPrice = productData[1].value;
+    const newProduct = `<tr class="list__product"><th class="product__name">${ProductName}</th><th class="product__price">${ProductPrice}</th><th><button type="button" class="change-button">&#9998</button><button type="button" class="delete-button">&#10006</button></th></tr>`;
+    newRowTable.insertAdjacentHTML('beforeend', newProduct);
+  }
+
+  static renderProducts() {
+    const products = document.getElementsByClassName('list__product');
+    for (const product of [...products]) {
+      product.remove();
+    }
+    const keys = Object.keys(localStorage);
+    const newRowTable = document.querySelector('.list__table > tbody');
+    for (const key of keys) {
+      const ProductName = JSON.parse(localStorage.getItem(key))[0].value;
+      const ProductPrice = JSON.parse(localStorage.getItem(key))[1].value;
       const newProduct = `<tr class="list__product"><th class="product__name">${ProductName}</th><th class="product__price">${ProductPrice}</th><th><button type="button" class="change-button">&#9998</button><button type="button" class="delete-button">&#10006</button></th></tr>`;
       newRowTable.insertAdjacentHTML('beforeend', newProduct);
     }
--- src/js/Logic.js
+++ src/js/Logic.js
@@ -10,24 +10,42 @@ export default class Logic {
       this.listenerOfAdd();
       this.listenerOfSave();
       this.listenerOfReset();
+      this.listenerOfСhange();
+      this.listenerOfDelete();
     }
   }
 
   static startRender() {
-    if (localStorage.productData) {
-      DOM.renderNewProduct(JSON.parse(localStorage.productData));
+    if (localStorage) {
+      DOM.renderProducts();
     }
   }
 
   listenerOfAdd() {
-    this.element.addEventListener('click', (e) => DOM.renderPopUp(e));
+    this.element.addEventListener('click', (e) => DOM.add(e));
   }
 
   listenerOfSave() {
-    this.element.addEventListener('click', (e) => DOM.saveProduct(e));
+    this.element.addEventListener('click', (e) => DOM.saveProduct(e, this.index));
+    this.index = null;
   }
 
   listenerOfReset() {
     this.element.addEventListener('click', (e) => DOM.resetForm(e));
+  }
+
+  listenerOfСhange() {
+    this.element.addEventListener('click', (e) => Logic.changeProduct(e));
+  }
+
+  listenerOfDelete() {
+    this.element.addEventListener('click', (e) => DOM.deleteProduct(e));
+  }
+
+  static changeProduct(e) {
+    if (e.target.classList.contains('change-button')) {
+      DOM.renderPopUp();
+      DOM.completeForm(e);
+    }
   }
 }
